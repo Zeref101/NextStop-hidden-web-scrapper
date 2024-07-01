32a0f63bf243651256ab9eda043ba919f43e66a0
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-01 23:59:17 +0530

    feat: Add function to retrieve places by state name

---
+++ app/services/get_places_by_state_name.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+
+
+def find_places_in_state_name(location: str):
+
+    if location:
+        location = location.replace(" ", "-")
+    url = f"https://www.holidify.com/state/{location}/top-destinations-places-to-visit.html"
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    places_divs = soup.find_all('div', 'col-12 col-md-6 pr-md-3')
+
+    # print(places_divs)
+    places_info = []
+
+    for div in places_divs:
+        title = div.find('h3', class_='card-heading').text.strip()
+        title = re.sub(r'^\d+\.\s*', '', title)
+
+        img_url = div.find('img', class_='card-img-top')['data-original']
+
+        description = div.find('p', class_='card-text').text.strip()
+
+        places_dict = {
+            'title': title,
+            'img_url': img_url,
+            'description': description
+        }
+
+        places_info.append(places_dict)
+
+    return places_info
