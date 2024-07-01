5424772e10c644b58c78a96e1bc07b29bccd4cc4
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-01 22:14:32 +0530

    chore: Include router for places to visit in main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.routes.places_to_visit import router as placesToVisit_router
 
 app = FastAPI()
 
@@ -14,6 +15,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+app.include_router(placesToVisit_router)
 
 
 @app.get("/")
